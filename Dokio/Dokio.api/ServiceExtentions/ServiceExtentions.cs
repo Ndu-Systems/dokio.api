e3670af81d41e0090a43d0b4bc34451eea3722c1
freedomkhanyile <freedom.khanyile1@gmail.com>
2018-11-26 11:19:20 +0200

    initial commit with loggerservice implementation

---
+++ Dokio/Dokio.api/ServiceExtentions/ServiceExtentions.cs
@@ -0,0 +1,39 @@
+﻿using Dokio.Contracts.ILoggerService;
+using Dokio.LoggerService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dokio.api.ServiceExtentions
+{
+    public static class ServiceExtentions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                               .AllowAnyHeader()
+                               .AllowAnyMethod()
+                               .AllowCredentials());
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
+
+        public static void  ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+    }
+}
