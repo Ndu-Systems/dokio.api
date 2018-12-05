1fbf7d812ecd2f14d9263bbead0188122f7d5d61
freedomkhanyile <freedom.khanyile1@gmail.com>
2018-12-05 09:31:45 +0200

    Unit test completed with assert

--- Dokio/Dokio.Tests/Patients/PatientsControllerTests.cs
+++ Dokio/Dokio.Tests/Patients/PatientsControllerTests.cs
@@ -9,6 +9,7 @@ using Dokio.Tests.Patients.FakeService;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Moq;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,13 +19,7 @@ using Xunit;
 namespace Dokio.Tests.Patients
 {
     public class PatientsControllerTests
-    {
-        //Use the tripple AAA principle or AA for Get Calls.
-        //A = Arrange , A = Act , A = Arrange
-        PatientsController _patientsController;        
-        
-        IPatientRepository _patient;
-         
+    {             
 
         [Fact]
         public void GetAllPatients_WhenCalled_ReturnOKResults()
@@ -49,24 +44,80 @@ namespace Dokio.Tests.Patients
             mockWrapper.Setup(mw => mw.Patient).Returns(mockRepository.Object);
 
             //Act 
-            var actual = new PatientsController(mockLoggerService.Object, mockWrapper.Object);
-            var result = actual.GetAllPatients();
+            var controller = new PatientsController(mockLoggerService.Object, mockWrapper.Object);
+            var result = controller.GetAllPatients();
+
 
             //Assert
-             
-        }
-       
+            var okObjectResults = result as OkObjectResult;
+            Assert.NotNull(okObjectResults);
+
+            var patientsList = okObjectResults.Value;
+            Assert.NotNull(patientsList);
+        }           
 
 
         public IQueryable<Patient> ListOfPatients()
         {
            var _patients = new List<Patient>()
             {
-                new Patient() {Id = new Guid("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200"), FirstName="FakeJon", Surname = "FakeDoe", Email = "FakeJon@mail.com", StatusId = 1},
-                new Patient() {Id = new Guid("2584bafa-1cce-4e81-9009-1611bab71bfc"), FirstName="FakeKing", Surname = "FakeJames", Email = "FakeKing@mail.com", StatusId = 1},
-                new Patient() {Id = new Guid("3a097051-b6b0-474f-aeda-ac8fe800b3eb"), FirstName="FakeBen", Surname = "FakeRich", Email = "FakeBen@mail.com", StatusId = 2},
-                new Patient() {Id = new Guid("ee7b83be-5257-4716-8290-b082fdab663b"), FirstName="FakeGin", Surname = "FakeKazama", Email = "FakeGin@mail.com", StatusId = 1},
-
+                new Patient() {
+                    Id = new Guid("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200"),
+                    FirstName ="FakeJon",
+                    Surname = "FakeDoe",
+                    Email = "FakeJon@mail.com",
+                    IdNumber = "0000000000000" ,
+                    DOB ="000000", Gender ="Male",
+                    AddressLine1 ="1",
+                    AddressLine2 ="Main 2",
+                    AddressLine3 ="300",
+                    City ="M-City",
+                    PostCode ="1234568",
+                    GlobalKey ="cce1d13f",
+                    Cellphone ="0987456123",
+                    CreateUserId =1,
+                    CreateDate = DateTime.Now.Date,
+                    ModifyUserId = 1,
+                    ModifyDate = DateTime.Now.Date,
+                    StatusId = 1},
+                 new Patient() {
+                    Id = new Guid("fc31c904-ea43-42e3-826b-b2313e0b4d68"),
+                    FirstName ="King",
+                    Surname = "Shine",
+                    Email = "kingshine@mail.com",
+                    IdNumber = "0000000000000" ,
+                    DOB ="000000", Gender ="Male",
+                    AddressLine1 ="1",
+                    AddressLine2 ="Rail 2",
+                    AddressLine3 ="5005",
+                    City ="M-City",
+                    PostCode ="1234568",
+                    GlobalKey ="53ea0cd9c200",
+                    Cellphone ="0587456123",
+                    CreateUserId =1,
+                    CreateDate = DateTime.Now.Date,
+                    ModifyUserId = 1,
+                    ModifyDate = DateTime.Now.Date,
+                    StatusId = 1},
+                  new Patient() {
+                    Id = new Guid("03a98ce8-c141-48f6-b490-0ddba4074add"),
+                    FirstName ="Marry",
+                    Surname = "Anne",
+                    Email = "marry@mail.com",
+                    IdNumber = "0000000000000" ,
+                    DOB ="000000", Gender ="Female",
+                    AddressLine1 ="1",
+                    AddressLine2 ="Rail 2",
+                    AddressLine3 ="5005",
+                    City ="M-City",
+                    PostCode ="1234568",
+                    GlobalKey ="ab2bd817",
+                    Cellphone ="0587456123",
+                    CreateUserId =1,
+                    CreateDate = DateTime.Now.Date,
+                    ModifyUserId = 1,
+                    ModifyDate = DateTime.Now.Date,
+                    StatusId = 1},
             }.AsQueryable();
 
             return _patients;
